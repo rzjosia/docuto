25f02dd6ec93a7fdd2132f16d21844746a9e9aa3
YannL98 <72266113+YannL98@users.noreply.github.com>
2021-03-05 11:04:56 +0100

    c'est le projet

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # docuto
+=======
+# ninja_trips
+
+A new Ninja Flutter project.
+>>>>>>> 870f4bf (c'est le projet)
---
+++ lib/models/Trip.dart
@@ -0,0 +1,8 @@
+class Trip {
+  final String title;
+  final String location;
+  final String price;
+  final String img;
+
+  Trip({this.title, this.location, this.price, this.img});
+}
